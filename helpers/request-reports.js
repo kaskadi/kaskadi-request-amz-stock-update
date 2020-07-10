a23ec7b6437abc183377a919ab3135179a5ca31f
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-10 17:01:30 +0200

    Updated environment variables

--- helpers/request-reports.js
+++ helpers/request-reports.js
@@ -4,9 +4,9 @@ module.exports = async (ids) => {
 
 async function requestReports(ids) {
   const MWS = require('mws-client')({
-    AWSAccessKeyId: process.env.ACCESS_KEY,
-    SellerId: process.env.SELLER_ID,
-    MWSAuthToken: process.env.MWS_AUTH_TOKEN
+    AWSAccessKeyId: process.env.MWS_KEY_ID,
+    SellerId: process.env.AMZ_EU_SELLER_ID,
+    MWSAuthToken: process.env.MWS_KEY_SECRET
   })
   const reqOpts = {
     ReportType: '_GET_MERCHANT_LISTINGS_ALL_DATA_',
